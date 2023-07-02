576b432bf22861248beccc34deda5c834b9f6bf7
daniel.cao <caoyue_8@163.com>
2023-07-02 19:11:37 +0800

    Ignore bin directory

--- boards/RAK3183/examples/LoRaWAN_ISM2400/keil/bin/smtc_hal_rng.d
+++
@@ -1,6 +0,0 @@
-.\bin\smtc_hal_rng.o: ..\src\smtc_hal_rng.c
-.\bin\smtc_hal_rng.o: F:\KEIL5\ARM\ARMCC\Bin\..\include\stdint.h
-.\bin\smtc_hal_rng.o: F:\KEIL5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\bin\smtc_hal_rng.o: F:\KEIL5\ARM\ARMCC\Bin\..\include\stdio.h
-.\bin\smtc_hal_rng.o: F:\KEIL5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\bin\smtc_hal_rng.o: ..\src\smtc_hal_rng.h
